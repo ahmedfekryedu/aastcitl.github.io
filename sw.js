f87aad7d98726fd3ee2e2317c6e005460c9037c8
ahmedfekryedu <ahmedmfekry@aast.edu>
2025-12-28 11:23:34 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker Registered');
-});
-
-self.addEventListener('fetch', (e) => {
-  // هذا الكود يسمح للتطبيق بالعمل بسلاسة
-  e.respondWith(fetch(e.request));
-});
